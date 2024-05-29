d530454cfade141eadb8c58cf735750bf30bd83e
Abhimanyu kumar <abhimanyu392kumar@gmail.com>
2024-05-29 21:26:59 +0530

    Angular sasic code bkp

---
+++ Angular-Practice/src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './basics/app.component.html',
+  styleUrl: './basics/app.component.css'
+})
+export class AppComponent {
+  title = 'Angular-Practice';
+  course:string = "Angular";
+  isInvalid:boolean = false;
+  // Style binding
+  randomColor  = this.getRandomColor();
+  heading:string = 'main heading';
+  hasError:boolean = true;
+  isspeical:boolean = true;
+  style1:string = 's1';
+
+
+
+  //#####################################################
+  public getRandomColor(){
+    var letter = '0123456789ABCDEFG'.split('');
+    var color = '#';
+    for(var i = 0 ; i < 6 ; i++){
+        color += letter[Math.floor(Math.random()*16)];
+    }
+    return color;
+  }
+
+  // function to set new random color
+  setColor(){
+    this.randomColor = this.getRandomColor();
+  }
+
+}
